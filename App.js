6618c63e5e1e6feb134e043b0705873c246e4d8b
goesrafa <rafa.goes.2012@gmail.com>
2021-02-25 14:58:31 -0300

    Inicial

--- App.js
+++ App.js
@@ -1,21 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'; //UseState é um React Hook
+import { SafeAreaView, Text, StyleSheet, Image, ActivityIndicator } from 'react-native';
+import clarice from './assets/clarice.png'
+import { FontAwesome } from '@expo/vector-icons'
+import * as Speech from 'expo-speech'
 
 export default function App() {
+  const [falando, setFalando] = useState(false)
+  let frases = [
+    "Até cortar os próprios defeitos pode ser perigoso. Nunca se sabe qual é o defeito que sustenta nosso edifício inteiro.",
+    "Que ninguém se engane, só se consegue a simplicidade através de muito trabalho.",
+    "Tenho várias caras. Uma é quase bonita, outra é quase feia. Sou um o quê? Um quase tudo.",
+    "A gente tem o direito de deixar o barco correr. As coisas se arranjam, não é preciso empurrar com tanta força.",
+    "Amar não acaba. É como se o mundo estivesse a minha espera. E eu vou ao encontro do que me espera.",
+    "Mas há a vida que é para ser intensamente vivida. Há o amor. Que tem que ser vivido até a última gota. Sem nenhum medo. Não mata.",
+    "Toda mulher leva um sorriso no rosto e mil segredos no coração.",
+    "Não tenho tempo pra mais nada, ser feliz me consome muito.",
+    "Ela acreditava em anjo e, porque acreditava, eles existiam."
+  ]
+  const iniciaFala = () => {
+    setFalando(true)
+  }
+
+  const finalizaFala = () => {
+    setFalando(false)
+  }
+
+  function falar() {
+    let frase = frases[parseInt(Math.random() * frases.length)]
+    Speech.speak(frase, {
+      language: 'pt',
+      onStart: iniciaFala,
+      onDone: finalizaFala
+    })
+  }
+
+
+  function parar() {
+    Speech.stop()
+  }
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.Principal}>
+      <Text style={styles.Titulo}>
+        <FontAwesome name="comments-o" size={50} color="#8A2BE2" />&nbsp;
+        Fala Clarice!
+      </Text>
+      <Image source={clarice} style={styles.Imagem}></Image>
+
+      {falando && <ActivityIndicator size='large' color="#4B0082" />}
+
+      <FontAwesome.Button
+        name="volume-up"
+        backgroundColor="#9370DB"
+        style={styles.Botao}
+        onPress={falar}>Ouvir Frase
+      </FontAwesome.Button>
+
+      <FontAwesome.Button
+        name="stop-circle"
+        onPress={parar}
+        backgroundColor={!falando ? "#778899": "#4B0082"}
+        disabled={!falando}
+        style={styles.Botao}>Parar
+        
+      </FontAwesome.Button>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  Principal: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   },
+  Titulo: {
+    color: '#A020F0',
+    fontSize: 30
+  },
+  Imagem: {
+    height: 400,
+    width: '80%',
+    resizeMode: 'center'
+  },
+  Botao: {
+    width: 150,
+    height: 30,
+    borderRadius: 20
+  }
+
+
+
 });
